9136262870d41d7cd9341feff3649cbcfdb128fa
Ito Shoma <stealpattan@gmail.com>
2020-04-30 13:55:35 +0900

    サービス追加

---
+++ app/Http/Service/HttpConnect.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace APP\Http\Service;
+
+class HttpConnect{
+	public function getTest(){
+		// initialize client
+		$client = new \GuzzleHttp\Client();
+		// execute request
+		$response = $client->request('GET', 'https://www.hoehoe');
+		// return
+		return $response->getBody();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
